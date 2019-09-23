318ea0defe9eb1ee6955cc47efe9e436afaf72c4
seike <seike@email.com>
2019-09-23 11:17:49 +0000

    TOPの最終調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
+  
+  #ユーザー登録画面
   get '/signup', to: 'users#new'
   
+  #ログイン画面
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
