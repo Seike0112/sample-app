68da52037821a41c26c4e409fea6ea76a1f1e803
seike <seike@email.com>
2019-09-23 14:03:47 +0000

    各html作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,31 @@
 class TasksController < ApplicationController
-  def new
+  def index
+    @task_names = Task.all
   end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @task
+    else
+      render :new
+    end
+  end
+  
+  
+    private
+
+    def task_params
+      params.require(:task).permit(:task_name, :note)
+    end
+    
 end
